8d52973524d64d1463f109eeb4fe8f62d3aa4a56
Omar Selaya <201900837@est.umss.edu>
2024-11-26 00:44:39 -0400

    method to create post

--- src/app/posts/services/post.service.ts
+++ src/app/posts/services/post.service.ts
@@ -1,15 +1,16 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Post } from '../models/post';
+import { AuthService } from '../../authentication/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
   private baseUrl = 'http://localhost:9090/api/v1'; // URL base del backend
-  private reqHeader = { headers: new HttpHeaders({ 'Authorization': 'Bearer '+localStorage.getItem('token') }) };
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
   // Método para obtener los datos de una institución
   getInstitution(uuid: string): Observable<any> {
@@ -19,9 +20,14 @@ export class PostService {
 
   // Método para obtener los posts
   getPosts(): Observable<any>{
-    const urlPosts = 'post'
-    return this.http.get<any>(`${this.baseUrl}/${urlPosts}`);
+    const getPosts = 'post'
+    return this.http.get<any>(`${this.baseUrl}/${getPosts}`);
   }
 
-
+  //Método para crear un post
+  createPost(dataPost: Post): Observable<Post>{
+    const reqHeader = { headers: new HttpHeaders({ 'Authorization': 'Bearer '+this.authService.getToken() }) };
+    const createPost = 'post'
+    return this.http.post<Post>(`${this.baseUrl}/${createPost}`, dataPost, reqHeader)
+  }
 }
