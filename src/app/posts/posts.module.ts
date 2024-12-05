da19003c9a8fa2e508e5ca62950fe7a3be734685
Diego Placido Acosta Torrez <diegoacosta9239@gmail.com>
2024-12-05 11:24:38 -0400

    comments fitrst commit

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Importar FormsModule
 import { ViewAllPostsComponent } from './components/view-all-posts/view-all-posts.component';
 import { DepartmentDetailsComponent } from './components/department-details/department-details.component';
 import { PostComponent } from './components/post/post.component';
-
-
+import { CommentsComponent } from './components/comments/comments.component';
 
 @NgModule({
   declarations: [
     ViewAllPostsComponent,
     DepartmentDetailsComponent,
-    PostComponent
+    PostComponent,
+    CommentsComponent
   ],
-  exports: [ViewAllPostsComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule // Agregar FormsModule aquí
+  ],
+  exports: [ViewAllPostsComponent]
 })
 export class PostsModule { }
