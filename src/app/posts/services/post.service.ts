9f7d95d5c76a46f25e566c16554838bd0e6da323
RodrigoGutierrez <rodrigo.gutierrez.acc@gmail.com>
2025-06-02 01:16:12 -0400

    Added switch to allow publishing in facebook

--- src/app/posts/services/post.service.ts
+++ src/app/posts/services/post.service.ts
@@ -105,14 +105,12 @@ export class PostService {
       return this.http.post<FbUploadedMedia>(`${this.GRAPH_API_URL}/${this.FACEBOOK_PAGE_ID}/${resource}?description=${description}&access_token=${this.FACEBOOK_PAGE_ACCESS_TOKEN}`, formData);
   }
 
-  //Método para subir imagenes a facebook
+  //Método para subir documentos a facebook
    publishDocumentToFacebook(formData: FormData, description: string, linkDoc: string): Observable<FbUploadedMedia> {
         const resource = 'feed';
-        linkDoc = 'http://imagenes.fcyt.umss.edu.bo/Calendario%20academico-2025.pdf';
+        //linkDoc = 'http://imagenes.fcyt.umss.edu.bo/Calendario%20academico-2025.pdf';
         return this.http.post<FbUploadedMedia>(`${this.GRAPH_API_URL}/${this.FACEBOOK_PAGE_ID}/${resource}?message=${description}&link=${linkDoc}&access_token=${this.FACEBOOK_PAGE_ACCESS_TOKEN}`, formData);
     }
-
-
 
   //Método para subir media (imagenes y videos)
   uploadMedia(formData: FormData): Observable<UploadedMedia[]> {
