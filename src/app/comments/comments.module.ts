7d3cc0617bb4857e4c262f3c83202a11e6520567
Omar Selaya <201900837@est.umss.edu>
2025-06-02 19:43:51 -0400

    mostrar reacciones de comentarios

--- src/app/comments/comments.module.ts
+++ src/app/comments/comments.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
 import { ReactionsComponent } from './components/reactions/reactions.component';
+import { ModalListReactionsCommentsComponent } from './components/modal-list-reactions-comments/modal-list-reactions-comments.component';
 
 
 
 @NgModule({
   declarations: [
- 
-    ReactionsComponent 
+    ReactionsComponent,
+    ModalListReactionsCommentsComponent 
   ],
   imports: [
     CommonModule
   ],
   exports: [
-   
     ReactionsComponent
   ]
 })
